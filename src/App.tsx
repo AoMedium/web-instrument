c2c9dba3ad862f3751c5e2a49945faa37f95c8d0
ochu761 <75078332+AoMedium@users.noreply.github.com>
2023-04-21 20:27:58 +1200

    add tone.js

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import KeyGroup from './components/KeyGroup';
 import LargeKey from './components/LargeKey';
 import { Edge } from './domain/Constants';
 import Note from './domain/Note';
+import {playSynth} from "./audio";
 
 function App() {
 
   return (
     <>
+      <button onClick={playSynth}></button>
       <KeyGroup position={[Edge.TOP, Edge.LEFT]}>
         <LargeKey note={new Note("G#")} />
         <LargeKey note={new Note("G#")} />
