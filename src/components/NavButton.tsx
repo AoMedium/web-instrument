89b6f288826e81a7692324194bd1efe2725d9c7e
ochu761 <75078332+AoMedium@users.noreply.github.com>
2023-04-24 17:34:38 +1200

    tidy up styles

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -4,7 +4,7 @@ import styles from './NavButton.module.css';
 
 type Props = {
   to: string
-  children: string
+  children: JSX.Element | string
 }
 
 export default function NavButton(props: Props) {
--- src/pages/InstrumentPanel/InstrumentPanel.tsx
+++ src/pages/InstrumentPanel/InstrumentPanel.tsx
@@ -1,4 +1,3 @@
-import { NavLink } from "react-router-dom";
 import Instrument from "./Instrument";
 import NavButton from "../../components/NavButton";
 
