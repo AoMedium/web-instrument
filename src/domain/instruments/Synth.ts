ef8dde369f33a985aebdcedfb30a198a251351a2
ochu761 <75078332+AoMedium@users.noreply.github.com>
2023-05-05 17:10:11 +1200

    add ToneHandler singleton

--- src/domain/instruments/Synth.ts
+++ src/domain/instruments/Synth.ts
@@ -1,7 +1,23 @@
+import * as Tone from "tone";
 import InstrumentType from "./InstrumentType";
 
 export default class Synth extends InstrumentType {
-  constructor() {
-    super("Synth");
+
+  private type: string;
+  
+  constructor(type?: "sine" | "square" | "triangle" | "sawtooth") {
+    super("Synth", new Tone.PolySynth(Tone.Synth, {
+      oscillator: {
+        type: "triangle"
+      },
+      envelope: { // FIXME: lowered volume during sustain
+        attack: 0.005,
+        decay: 0,
+        sustain: 1,
+        release: 3
+      }
+    }).toDestination());
+    
+    this.type = type || "triangle";
   }
 }
